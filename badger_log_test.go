d0249e72d67bbddfbb2b8c41e66d7e591c91370b
Marco <marco.helmich@live.com>
2018-07-18 12:03:39 -0700

    - fixing tests by changing thresholds

--- badger_log_test.go
+++ badger_log_test.go
@@ -28,6 +28,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func BenchmarkBadgerLogAppend(b *testing.B) {
+	dir := "./BenchmarkBadgerLogAppend/"
+	assert.Nil(b, os.RemoveAll(dir))
+	cc := newMockCopyCat()
+	log, err := newBadgerLog(dir, "T1", "01CFRDSD7PBQZXV8N515RVYTZQ", cc)
+	assert.Nil(b, err)
+	defer log.close()
+
+	var offset uint64
+	keyPrefix := "key_"
+	valuePrefix := "value_"
+	offsets := make([]uint64, 0)
+	for i := 0; i < b.N*1000; i++ {
+		key := []byte(keyPrefix + strconv.Itoa(i))
+		value := []byte(valuePrefix + strconv.Itoa(i))
+		offset, err = log.append(key, value)
+		assert.Nil(b, err)
+		offsets = append(offsets, offset)
+	}
+	assert.Nil(b, os.RemoveAll(dir))
+}
+
 func TestBadgerLogAppendReadBasic(t *testing.T) {
 	var err error
 	var log *badgerLog
@@ -39,7 +61,7 @@ func TestBadgerLogAppendReadBasic(t *testing.T) {
 	assert.Nil(t, err)
 	defer log.close()
 
-	numEntries := 1111
+	numEntries := 111
 	keyPrefix := "key_"
 	valuePrefix := "value_"
 	offsets := make([]uint64, 0)
@@ -78,7 +100,7 @@ func TestBadgerLogCompaction(t *testing.T) {
 	assert.Nil(t, err)
 	defer log.close()
 
-	numEntries := 1111
+	numEntries := 111
 	keyPrefix := "key_"
 	valuePrefix := "value_"
 	offsets := make([]uint64, 0)
@@ -96,7 +118,7 @@ func TestBadgerLogCompaction(t *testing.T) {
 	assert.Equal(t, numEntries, countLogEntries(log))
 	assert.Equal(t, numEntries, log.currentLogSize)
 
-	log.desiredLogSize = 100
+	log.desiredLogSize = 30
 	log.maybeCompact()
 	assert.Equal(t, log.desiredLogSize+1, countLogEntries(log))
 	assert.Equal(t, numEntries-log.desiredLogSize, countEntriesInState(log))
@@ -111,7 +133,7 @@ func TestBadgerLogCompaction(t *testing.T) {
 		offsets = append(offsets, offset)
 	}
 
-	assert.Equal(t, numEntries+101, countLogEntries(log))
+	assert.Equal(t, numEntries+31, countLogEntries(log))
 
 	assert.Nil(t, log.close())
 	assert.Nil(t, os.RemoveAll(dir))
@@ -128,9 +150,9 @@ func TestBadgerLogSnapshotProviderConsumer(t *testing.T) {
 	assert.Nil(t, err)
 	defer log1.close()
 
-	log1.desiredLogSize = 300
+	log1.desiredLogSize = 30
 
-	numEntries := 1111
+	numEntries := 111
 	keyPrefix := "key_"
 	valuePrefix := "value_"
 	offsets := make([]uint64, 0)
@@ -145,9 +167,9 @@ func TestBadgerLogSnapshotProviderConsumer(t *testing.T) {
 	}
 
 	assert.True(t, len(offsets) > 0)
-	assert.Equal(t, 601, countEntriesInState(log1))
+	assert.Equal(t, 61, countEntriesInState(log1))
 	// HACK - compaction ran therefore we have our reserved key in there as well
-	assert.Equal(t, 510+1, countLogEntries(log1))
+	assert.Equal(t, 51, countLogEntries(log1))
 
 	bites, err := log1.snapshotProvider()
 	assert.Nil(t, err)
@@ -155,8 +177,8 @@ func TestBadgerLogSnapshotProviderConsumer(t *testing.T) {
 	logSnap := &pb.LogSnapshot{}
 	err = logSnap.Unmarshal(bites)
 	assert.Nil(t, err)
-	assert.Equal(t, 510, len(logSnap.Log))
-	assert.Equal(t, 601, len(logSnap.State.M))
+	assert.Equal(t, 50, len(logSnap.Log))
+	assert.Equal(t, 61, len(logSnap.State.M))
 
 	var log2 *badgerLog
 	log2, err = newBadgerLog(dir, "T2", "01CFRDSD7PBQZXV8N515RVYTZQ", cc)
@@ -169,9 +191,9 @@ func TestBadgerLogSnapshotProviderConsumer(t *testing.T) {
 
 	// wait for the log to load itself from the snapshot
 	time.Sleep(500 * time.Millisecond)
-	assert.Equal(t, 601, countEntriesInState(log2))
+	assert.Equal(t, 61, countEntriesInState(log2))
 	// HACK - compaction ran therefore we have our reserved key in there as well
-	assert.Equal(t, 510+1, countLogEntries(log2))
+	assert.Equal(t, 51, countLogEntries(log2))
 
 	assert.Nil(t, os.RemoveAll(dir))
 }
